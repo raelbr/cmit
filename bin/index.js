38cea7a4aec957d6933b53a6beae00a1fc30692f
Rael <rael82@gmail.com>
2021-02-16 16:01:24 -0300

    fix(core): Package changes

--- bin/index.js
+++ bin/index.js
@@ -1,8 +1,6 @@
 #!/usr/bin/env node
 const { refresh } = require("../lib/utils/interface");
 const { displayMainOptions } = require("../lib/core/main");
-const log = require("../lib/utils/log");
 
 refresh();
 displayMainOptions();
-// log.success("Started with success");
